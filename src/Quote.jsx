4ad7908b18bda89f82296112927f04b181d1a52c
Doruk Aytekin <horationelson1903@gmail.com>
2023-10-07 13:47:12 -0700

    add color change feature

--- src/Quote.jsx
+++ src/Quote.jsx
@@ -3,6 +3,26 @@ import Quotes from "../src/Quotes.json"
 export default class Quote extends React.Component {
     //renk ayarlamasini yap rengini degistirmek istedin seylere bi class var color diye onu 
     //javascript document ile cek ve icindeki color rengini degistirmek istefigin renge ver
+    colors = [
+        '#16a085',
+        '#27ae60',
+        '#2c3e50',
+        '#f39c12',
+        '#e74c3c',
+        '#9b59b6',
+        '#FB6964',
+        '#342224',
+        '#472E32',
+        '#BDBB99',
+        '#77B1A9',
+        '#73A857',
+        "#232D3F",
+        "#93B1A6",
+        "#0E2954",
+        "#0A2647",
+        "#E7F6F2",
+        "#2D4263"
+      ];
     constructor(props) {
         let lastIndex = 0;
         super(props);
@@ -14,13 +34,17 @@ export default class Quote extends React.Component {
     }
     handleClick(){
         let index = Math.floor(Math.random() * Quotes.quotes.length);
-        console.log("index"+index)
+        let color = Math.floor(Math.random() * this.colors.length)
+        console.log(color)
         if(this.lastIndex != index){
-            
             this.setState({
                 quote:Quotes.quotes[index].quote,
                 author:Quotes.quotes[index].author
             })   
+            document.body.style.backgroundColor = this.colors[color];
+            document.getElementById("color1").style.color = this.colors[color];
+            document.getElementById("color2").style.color = this.colors[color];
+            
         }
         else{
             if(index == 0){
@@ -36,21 +60,22 @@ export default class Quote extends React.Component {
                 quote:Quotes.quotes[index].quote,
                 author:Quotes.quotes[index].author
             })  
+            document.body.style.backgroundColor = this.colors[color];
+            document.getElementById("color1").style.color = this.colors[color];
+            document.getElementById("color2").style.color = this.colors[color];
         }   
         this.lastIndex = index;
         
     }
     render(){
-        console.log("lastindex"+this.lastIndex)
-        
         return(
             <div >
               <h1 className="center title">Random Quote Machine</h1>
               <br />
               <p className="center developer">Developed By Doruk Aytekin</p>
                 <div className="quote-div">
-                    <p className="center quote-p">{this.state.quote}</p>
-                    <p className="center">{this.state.author}</p>
+                    <p id="color1" className="center quote-p">{this.state.quote}</p>
+                    <p id="color2" className="center">{this.state.author}</p>
                     <button  className="right button" onClick={this.handleClick}>New Quote</button>
                 </div>  
             </div>
